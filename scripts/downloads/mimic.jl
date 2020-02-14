63d2b8f6cb3ea1599acc18b86d916d0747afd3ee
natgeo-wong <natgeo.wong@outlook.com>
2020-02-14 03:22:44 -0500

    Changed MIMIC downloads and Project.toml files

--- scripts/downloads/mimic.jl
+++ scripts/downloads/mimic.jl
@@ -8,14 +8,14 @@ user = "natgeo.wong@outlook.com";
 rvec = ["TRP","ISM"]; rext = ["SEA","MLD","SMT"];
 ddir = "/n/kuangdss01/lab/clisat/"
 
-for yr = 2001 : 2019, mo = 1 : 12
+for yr = 2017 : 2019, mo = 1 : 12
     clisatdownload("mtpw2m",Date(yr,mo),email=user,regions=rvec,path=ddir);
 end
 
-for yr = 2001 : 2019, mo = 1 : 12, reg in rext
+for yr = 2017 : 2019, mo = 1 : 12, reg in rext
     clisatsubregion("mtpw2m",Date(yr,mo),region=reg,path=ddir);
 end
 
-for yr = 2001 : 2019, reg in vcat(rvec,rext)
+for yr = 2017 : 2019, reg in vcat(rvec,rext)
     clisatanalysis("mtpw2m",yr,varname="tpw",region=reg,path=ddir);
 end
