e02369aa7d50477ab9539490b806e99014f0a38c
natgeo-wong <natgeo.wong@outlook.com>
2020-02-15 23:04:03 -0500

    Updating Project.toml and era5 download scripts

--- scripts/downloads/era5-TRP.jl
+++ scripts/downloads/era5-TRP.jl
@@ -6,60 +6,60 @@ global_logger(ConsoleLogger(stdout,Logging.Info))
 
 
 ## Dry Surface
-init,eroot = erastartup(1,1);
+init,eroot = erastartup(1,1,"/n/kuangdss01/lab/ecmwf/");
 emod,epar,ereg,etime = erainitialize(1,1,2,0,init);
 eradownload(emod,epar,ereg,etime,eroot)
 
-init,eroot = erastartup(1,1);
+init,eroot = erastartup(1,1,"/n/kuangdss01/lab/ecmwf/");
 emod,epar,ereg,etime = erainitialize(1,2,2,0,init);
 eradownload(emod,epar,ereg,etime,eroot)
 
-init,eroot = erastartup(1,1);
+init,eroot = erastartup(1,1,"/n/kuangdss01/lab/ecmwf/");
 emod,epar,ereg,etime = erainitialize(1,3,2,0,init);
 eradownload(emod,epar,ereg,etime,eroot)
 
-init,eroot = erastartup(1,1);
+init,eroot = erastartup(1,1,"/n/kuangdss01/lab/ecmwf/");
 emod,epar,ereg,etime = erainitialize(1,5,2,0,init);
 eradownload(emod,epar,ereg,etime,eroot)
 
 
 ## Dry Pressure Levels
-init,eroot = erastartup(1,1);
+init,eroot = erastartup(1,1,"/n/kuangdss01/lab/ecmwf/");
 emod,epar,ereg,etime = erainitialize(2,1,2,0,init);
 eradownload(emod,epar,ereg,etime,eroot)
 
-init,eroot = erastartup(1,1);
+init,eroot = erastartup(1,1,"/n/kuangdss01/lab/ecmwf/");
 emod,epar,ereg,etime = erainitialize(2,2,2,0,init);
 eradownload(emod,epar,ereg,etime,eroot)
 
-init,eroot = erastartup(1,1);
+init,eroot = erastartup(1,1,"/n/kuangdss01/lab/ecmwf/");
 emod,epar,ereg,etime = erainitialize(2,3,2,0,init);
 eradownload(emod,epar,ereg,etime,eroot)
 
-init,eroot = erastartup(1,1);
+init,eroot = erastartup(1,1,"/n/kuangdss01/lab/ecmwf/");
 emod,epar,ereg,etime = erainitialize(2,4,2,0,init);
 eradownload(emod,epar,ereg,etime,eroot)
 
 
 ## Moist Surface
-init,eroot = erastartup(1,1);
+init,eroot = erastartup(1,1,"/n/kuangdss01/lab/ecmwf/");
 emod,epar,ereg,etime = erainitialize(3,1,2,0,init);
 eradownload(emod,epar,ereg,etime,eroot)
 
-init,eroot = erastartup(1,1);
+init,eroot = erastartup(1,1,"/n/kuangdss01/lab/ecmwf/");
 emod,epar,ereg,etime = erainitialize(3,2,2,0,init);
 eradownload(emod,epar,ereg,etime,eroot)
 
-init,eroot = erastartup(1,1);
+init,eroot = erastartup(1,1,"/n/kuangdss01/lab/ecmwf/");
 emod,epar,ereg,etime = erainitialize(3,3,2,0,init);
 eradownload(emod,epar,ereg,etime,eroot)
 
-init,eroot = erastartup(1,1);
+init,eroot = erastartup(1,1,"/n/kuangdss01/lab/ecmwf/");
 emod,epar,ereg,etime = erainitialize(3,5,2,0,init);
 eradownload(emod,epar,ereg,etime,eroot)
 
 
 ## Moist Pressure Levels
-init,eroot = erastartup(1,1);
+init,eroot = erastartup(1,1,"/n/kuangdss01/lab/ecmwf/");
 emod,epar,ereg,etime = erainitialize(4,2,2,0,init);
 eradownload(emod,epar,ereg,etime,eroot)
