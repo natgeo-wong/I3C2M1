360936b83be1bb1b946702e17a3a1b07487f20ab
Nathanael Wong <natgeo.wong@outlook.com>
2020-02-29 12:47:00 -0500

    Added ETOPO analysis functions for filtering of islands and their sizes.

---
+++ scripts/filterlandsea/landmasstest.jl
@@ -0,0 +1,35 @@
+using DrWatson
+@quickactivate "I3CM"
+
+using ClimateSatellite
+using NCDatasets
+
+greg = "SEA"
+# include(srcdir("etopo.jl")); lsseg,areawgt = etoporeg(greg); idarray = unique(lsseg);
+# nID = length(idarray); table = zeros(Int32,nID,2)
+#
+# for iID = 1 : nID
+#     table[iID,1] = idarray[iID];
+#     table[iID,2] = sum(lsseg .== idarray[iID]);
+# end
+#
+# table = table[table[:,2].>24,:];
+
+info = Dict(); clisatinfo!(info,"gpmimerg");
+fola = clisatanafol(info,greg)
+fnc  = joinpath(fola,clisatananame(info,Date(2001),greg)); ds = Dataset(fnca);
+prcpyr = ds["domain_yearly_mean_hourly"].var[:];
+
+function testextract(IDArray::Array{<:Real,2},data::Array{<:Real,3})
+
+    nID = size(IDArray,1); dataout = zeros(Int16,nID,48)
+
+    for ihr = 1 : 48; datahr = @view data[:,:,ihr]
+        for iID = 1 : nID; dataID = @view datahr[lsseg .== table[iID,1]];
+            dataout[iID,ihr] = mean(dataID);
+        end
+    end
+
+end
+
+out = testextract(table,prcpyr);
---
+++ src/etopo.jl
@@ -0,0 +1,83 @@
+using DrWatson
+@quickactivate "I3CM"
+
+using Dates
+using NCDatasets
+using ImageSegmentation
+using GeoRegions
+
+function findetopo()
+    fetopo = joinpath(@__DIR__,"../data/raw/ETOPO1.grd")
+    if !isfile(fetopo); error("$(Dates.now()) - Please download the ETOPO1 file.") end
+end
+
+function etopoinit()
+    findetopo();
+    fetopo = joinpath(@__DIR__,"../data/raw/ETOPO1.grd")
+    fsegnc = joinpath(@__DIR__,"../data/etopo/etopo-GLB.nc")
+    detopo = joinpath(@__DIR__,"../data/etopo/");
+    if !isdir(detopo); mkpath(detopo) end
+    if !isfile(fsegnc); etoposeg(fetopo,fsegnc) end
+    return fetopo,fsegnc
+end
+
+function etopoweights(lon::Vector{<:Real},lat::Vector{<:Real})
+
+    nlon = length(lon); nlat = length(lat); wgt = ones(nlon,nlat)
+    for ilat = 1 : nlat, ilon = 1 : nlon; wgt[ilon,ilat] = wgt[ilon,ilat] * lat[ilat] end
+    return wgt
+
+end
+
+function etoposeg(fetopo::AbstractString,fsegnc::AbstractString)
+
+    eds = Dataset(fetopo,"r"); lon = eds["x"][:]*1.0; lat = eds["y"][:]*1.0;
+    oro = eds["z"][:]*1; oro[oro.<0] .= 0; oro[oro.>0] .= 1;
+    seg = fast_scanning(oro); lab = labels_map(seg); wgt = etopoweights(lon,lat)
+    close(eds);
+
+    sds = Dataset(fsegnc,"c");
+    nlon,nlat = size(lab); sds.dim["longitude"] = nlon; sds.dim["latitude"] = nlat;
+    att_lon = Dict("units"=>"degrees_east","long_name"=>"longitude");
+    att_lat = Dict("units"=>"degrees_north","long_name"=>"latitude");
+    att_lab = Dict("long_name"=>"land_segment_labels");
+    att_wgt = Dict("long_name"=>"area_weighted_by_latitude");
+
+    defVar(sds,"longitude",lon,("longitude",),attrib=att_lon)
+    defVar(sds,"latitude",lat,("latitude",),attrib=att_lat)
+    defVar(sds,"landsegments",lab,("longitude","latitude"),attrib=att_lab)
+    defVar(sds,"weightedarea",wgt,("longitude","latitude"),attrib=att_wgt)
+
+    close(sds);
+
+end
+
+function etoporegextract(gregID::AbstractString,fregnc::AbstractString)
+
+    fetopo,fsegnc = etopoinit(); eds = Dataset(fsegnc,"r");
+    lon = eds["longitude"][:]*1.0; lat = eds["latitude"][:]*1.0;
+    rlon,rlat,rinfo = gregiongridvec(gregID,lon,lat); iWE,iNS = rinfo["IDvec"];
+    lab = eds["landsegments"][iWE,iNS]; nlon = length(rlon); nlat = length(rlat);
+    wgt = etopoweights(rlon,rlat)
+
+    rds = Dataset(fregnc,"c");
+    nlon,nlat = size(lab); rds.dim["longitude"] = nlon; rds.dim["latitude"] = nlat;
+    att_lon = Dict("units"=>"degrees_east","long_name"=>"longitude");
+    att_lat = Dict("units"=>"degrees_north","long_name"=>"latitude");
+    att_lab = Dict("long_name"=>"land_segment_labels");
+    att_wgt = Dict("long_name"=>"area_weighted_by_latitude");
+
+    defVar(rds,"longitude",rlon,("longitude",),attrib=att_lon)
+    defVar(rds,"latitude",rlat,("latitude",),attrib=att_lat)
+    defVar(rds,"landsegments",lab,("longitude","latitude"),attrib=att_lab)
+    defVar(rds,"weightedarea",wgt,("longitude","latitude"),attrib=att_wgt)
+
+    close(rds)
+
+end
+
+function etoporeg(gregID::AbstractString)
+    fregnc = joinpath(@__DIR__,"../data/etopo/etopo-$(gregID).nc")
+    if !isfile(fregnc); etoporegextract(gregID,fregnc) end
+    rds = Dataset(fregnc,"r"); return rds["landsegments"][:]*1,rds["weightedarea"][:]*1.0
+end
