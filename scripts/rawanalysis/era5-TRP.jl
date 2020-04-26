dd210c697c89e11babe7276f084c2020f202ceb7
Nathanael Wong <natgeo.wong@outlook.com>
2020-04-25 22:34:09 -0400

    Stable(?)-ish version for downloads

--- scripts/rawanalysis/era5-TRP.jl
+++ scripts/rawanalysis/era5-TRP.jl
@@ -6,19 +6,20 @@ global_logger(ConsoleLogger(stdout,Logging.Info))
 include(srcdir("common.jl"))
 
 init,eroot = erastartup(aID=2,dID=1,path="/n/kuangdss01/lab/ecmwf/"); adderaparams();
+pre = [1000,950,850,700,600,500,350,250,150,70]
 
-eraanalysis(init,eroot,modID="msfc",parID="prcp_conv",regID="TRP");
-eraanalysis(init,eroot,modID="msfc",parID="prcp_ls",regID="TRP");
-eraanalysis(init,eroot,modID="msfc",parID="prcp_tot",regID="TRP");
-eraanalysis(init,eroot,modID="msfc",parID="tcw",regID="TRP");
+eraanalysis(init,eroot,modID="msfc",parID="prcp_conv",regID="TRP",plvls="sfc");
+eraanalysis(init,eroot,modID="msfc",parID="prcp_ls",regID="TRP",plvls="sfc");
+eraanalysis(init,eroot,modID="msfc",parID="prcp_tot",regID="TRP",plvls="sfc");
+eraanalysis(init,eroot,modID="msfc",parID="tcw",regID="TRP",plvls="sfc");
 
-eraanalysis(init,eroot,modID="mpre",parID="shum",regID="TRP");
+eraanalysis(init,eroot,modID="mpre",parID="shum",regID="TRP",plvls=pre);
 
-eraanalysis(init,eroot,modID="dsfc",parID="pre_sfc",regID="TRP");
-eraanalysis(init,eroot,modID="dsfc",parID="t_sfc",regID="TRP");
-eraanalysis(init,eroot,modID="dsfc",parID="u_sfc",regID="TRP");
-eraanalysis(init,eroot,modID="dsfc",parID="v_sfc",regID="TRP");
+eraanalysis(init,eroot,modID="dsfc",parID="pre_sfc",regID="TRP",plvls="sfc");
+eraanalysis(init,eroot,modID="dsfc",parID="t_sfc",regID="TRP",plvls="sfc");
+eraanalysis(init,eroot,modID="dsfc",parID="u_sfc",regID="TRP",plvls="sfc");
+eraanalysis(init,eroot,modID="dsfc",parID="v_sfc",regID="TRP",plvls="sfc");
 
-eraanalysis(init,eroot,modID="dpre",parID="t_air",regID="TRP");
-eraanalysis(init,eroot,modID="dpre",parID="u_air",regID="TRP");
-eraanalysis(init,eroot,modID="dpre",parID="v_air",regID="TRP");
+eraanalysis(init,eroot,modID="dpre",parID="t_air",regID="TRP",plvls=pre);
+eraanalysis(init,eroot,modID="dpre",parID="u_air",regID="TRP",plvls=pre);
+eraanalysis(init,eroot,modID="dpre",parID="v_air",regID="TRP",plvls=pre);
